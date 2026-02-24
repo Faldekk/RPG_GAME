f13f893b3db55494994bd3de52128f73a0674357
Faldekk <kuba.mikula@wp.pl>
2026-02-24 15:12:22 +0100

    Program.cs

--- RPG_GAME/Program.cs
+++ RPG_GAME/Program.cs
@@ -1,1 +1,39 @@
-﻿
+﻿using System;
+using RPG_GAME.App;
+
+namespace RPG_GAME
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.CursorVisible = false;
+            try
+            {
+                
+                const int width = 80;
+                const int height = 30;
+
+                if (Console.BufferWidth < width) Console.BufferWidth = width;
+                if (Console.WindowWidth < width) Console.WindowWidth = width;
+
+                if (Console.BufferHeight < height) Console.BufferHeight = height;
+                if (Console.WindowHeight < height) Console.WindowHeight = height;
+            }
+            catch
+            {
+            }
+
+            Console.Clear();
+
+            var game = new Game();
+            game.Run();
+
+       
+            Console.CursorVisible = true;
+            Console.ResetColor();
+            Console.SetCursorPosition(0, Math.Min(Console.CursorTop, Console.BufferHeight - 1));
+            Console.WriteLine();
+        }
+    }
+}
